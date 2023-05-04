17dbf8e816fa921d3f11a450dcbee7f633571dc1
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-05-04 10:53:37 +0530

    initilized x

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    int x;
+    int x=12;
     public static void main(String[] args) {
         Main m=new Main();
       System.out.println(m.x);
