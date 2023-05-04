e5909e9258e0fd2b7aa80e7ef3dc21b961c90421
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-05-04 11:07:41 +0530

    -initilized x

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     int x=12;
+    int y=11;
     public static void main(String[] args) {
         Main m=new Main();
       System.out.println(m.x);
